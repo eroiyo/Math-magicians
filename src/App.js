b2d4e244c890bc81f5a451cb6cbed697da230f1c
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-18 04:53:42 -0400

    indentation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
 const App = () => {
-   return (
-    <div className="App">
-      <div className="App-body">
-        <Calculator />
+  render()
+  {
+    return (
+      <div className="App">
+        <div className="App-body">
+          <Calculator />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
