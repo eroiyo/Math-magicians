d210175d85e66c4ed2e9afefda4744668b07f004
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-18 05:59:18 -0400

    importing only "Components" from react

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 const App = class extends Component {
-  render(){
-  return (
-    <div className="App">
+  render() {
+    return (
+      <div className="App">
       <div className="App-body">
         <Calculator />
       </div>
     </div>
-  );
+    );
   }
 };
 
