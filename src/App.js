79e80e04b11a35d173cc810886a478056f7b5c6f
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-18 04:50:10 -0400

    add a Return

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 const App = () => {
-  (
+   return (
     <div className="App">
       <div className="App-body">
         <Calculator />
