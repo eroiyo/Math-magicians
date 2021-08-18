602d0f7e8839d9e77b43a421e61112155adb10e9
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-18 04:41:30 -0400

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
-import React from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const App = () => {
 
-  /* eslint-enable */
-  render() {
     return (
       <div className="App">
         <div className="App-body">
@@ -16,7 +11,6 @@ class App extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const Calculator = class extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  
+
   render() {
     return (
       <div className="calculator">
