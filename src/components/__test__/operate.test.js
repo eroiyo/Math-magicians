3a16e98cee52c2289e62feff236276cb7eb7fafa
Caleb Anderson <buyananderson@gmail.com>
2021-08-26 11:14:09 +0200

    Test the others  oparations

--- src/components/__test__/operate.test.js
+++ src/components/__test__/operate.test.js
@@ -9,12 +9,16 @@ describe('Testing oparations', () => {
     const result = operate(5, 3, '-');
     expect(result).toBe('2');
   });
-  it('Should return the Substraction', () => {
-    const result = operate(5, 3, '-');
-    expect(result).toBe('2');
+  it('Should return the Multiplication', () => {
+    const result = operate(5, 3, 'x');
+    expect(result).toBe('15');
   });
-  it('Should return the Substraction', () => {
-    const result = operate(5, 3, '-');
-    expect(result).toBe('2');
+  it('Should return the Division', () => {
+    const result = operate(15, 3, '÷');
+    expect(result).toBe('5');
+  });
+  it('Should return the Modulation', () => {
+    const result = operate(10, 3, '%');
+    expect(result).toBe('1');
   });
 });
