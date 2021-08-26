8270f7aadb4c4c1b379db3d90c4100ccf4fa8ec0
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-26 11:21:20 -0400

    fix more linters

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,11 @@
 import './Css/Quote.css';
 
 const Quote = () => (
-  <blockquote>MATHEMATICS is not about numbers, equations, computations
-     or algorithms:  it is about UNDERSTANDING. -William Paul Thurston </blockquote>
+  <blockquote>
+    MATHEMATICS is not about numbers, equations, computations
+    or algorithms:  it is about UNDERSTANDING. -William Paul Thurston
+    {' '}
+  </blockquote>
 );
 
 export default Quote;
