af488eb3dce9406b4224f6762d571f04f16d0598
Caleb Anderson <buyananderson@gmail.com>
2021-08-26 11:11:53 +0200

    Test the Subtraction oparation

--- src/components/__test__/operate.test.js
+++ src/components/__test__/operate.test.js
@@ -1,8 +1,20 @@
 import operate from '../../logic/operate';
 
-discribe('Testing oparations', () => {
+describe('Testing oparations', () => {
   it('Should return the Addition', () => {
     const result = operate(2, 3, '+');
-    expect(result).toBe(5);
+    expect(result).toBe('5');
+  });
+  it('Should return the Substraction', () => {
+    const result = operate(5, 3, '-');
+    expect(result).toBe('2');
+  });
+  it('Should return the Substraction', () => {
+    const result = operate(5, 3, '-');
+    expect(result).toBe('2');
+  });
+  it('Should return the Substraction', () => {
+    const result = operate(5, 3, '-');
+    expect(result).toBe('2');
   });
 });
